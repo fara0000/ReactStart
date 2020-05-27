ccb56ca80868fa3a1452668f93f97f5a91bf4a4e
fara0000 <fexri_imanzade@mail.ru>
2020-05-27 23:31:41 +0400

    already send object to model, everything is done!

--- ReactStart/src/REST.js
+++ ReactStart/src/REST.js
@@ -6,13 +6,5 @@ export const sendPutRequest = (data, cb) => {
     xhr.setRequestHeader("Content-type", "application-json");
     xhr.send(dataInJson);
 
-    xhr.onload = function() {
-        if (xhr.status != 200) {
-            console.log(xhr.statusText);
-        } else {
-            const data = JSON.parse(xhr.response);
-            console.log(data);
-            cb(data);
-        }
-    }
+
 }
--- ReactStart/src/pages/ModalWindow/ModalWindow.jsx
+++ ReactStart/src/pages/ModalWindow/ModalWindow.jsx
@@ -9,43 +9,33 @@ class ModalWindow extends React.Component {
         super(props)
         
         this.state = {
-            name : '',
-            email : '',
-            age : '',
-            phone : ''
+            inputValues: {}
+            // name : '',
+            // email : '',
+            // age : '',
+            // phone : ''
         }
-        this.handleName = this.handleName.bind(this);
-        this.handleAge = this.handleAge.bind(this);
-        this.handleEmail = this.handleEmail.bind(this);
-        this.handlePhone = this.handlePhone.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        // this.handleName = this.handleName.bind(this);
+        // this.handleAge = this.handleAge.bind(this);
+        // this.handleEmail = this.handleEmail.bind(this);
+        // this.handlePhone = this.handlePhone.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
     }  
 
-    handleName(e) {
-        const value = e.target.value;
-        this.setState({userName: value})
-        console.log(this.state);
+    inputChangeHandler = (e) => {
+        const {name, value} = e.target;
+
+        this.setState(prevState => ({
+            inputValues : {
+            ...prevState.inputValues,
+            [name] : value
+            }
+        }));
     }
 
-    handleAge(e) {
-        const value = e.target.value;
-        this.setState({age:value})
-        console.log(this.state);
-    }
-
-    handleEmail(e) {
-        const value = e.target.value;
-        this.setState({email:value})
-        console.log(this.state);
-    }
-
-    handlePhone(e) {
-        const value = e.target.value;
-        this.setState({phone:value})
-        console.log(this.state);
-    }
-
-    handleSubmit() {
+    checkFormHandler = (e) => {
+        e.preventDefault();
+        console.log( this.state.inputValues );  
         const person = this.state;
         let object = {person}
         let formData = new FormData(document.forms.person);
@@ -54,50 +44,95 @@ class ModalWindow extends React.Component {
             object[key] = value;
         });
         console.log(object);
-        sendPutRequest(object)
+        sendPutRequest(object);
+        return false;
     }
+    // handleName(e) {
+    //     const value = e.target.value;
+    //     this.setState({userName: value})
+    //     console.log(this.state);
+    // }
+
+    // handleAge(e) {
+    //     const value = e.target.value;
+    //     this.setState({age:value})
+    //     console.log(this.state);
+    // }
+
+    // handleEmail(e) {
+    //     const value = e.target.value;
+    //     this.setState({email:value})
+    //     console.log(this.state);
+    // }
+
+    // handlePhone(e) {
+    //     const value = e.target.value;
+    //     this.setState({phone:value})
+    //     console.log(this.state);
+    // }
+
+    // handleSubmit() {
+        // const person = this.state;
+        // let object = {person}
+        // let formData = new FormData(document.forms.person);
     
+        // formData.forEach(function(value, key){
+        //     object[key] = value;
+        // });
+        // console.log(object);
+    //     sendPutRequest(object)
+    // }
+    
+
     render () {
-        const {
-            name,
-            email,
-            age,
-            phone
-        } = this.state
-        
+        // const {
+        //     name,
+        //     email,
+        //     age,
+        //     phone
+        // } = this.state
+
         return (
-            <form onClick = {this.handleSubmit}>
+            <form onClick = {this.handleSubmit} onSubmit={this.checkFormHandler}>
                 <div className="Login_Container" >
                     <h1>PROVIDE YOUR CREDITIONALS</h1>
                     <p className = {'login_paraghraph'}>Full name:</p>
                     <Input 
                         type = {'text'}
+                        name = 'login'
                         className = {'input_login'}
                         placeholder = {'Enter your full name...'}
-                        onChange = {this.handleName}
+                        onChange={this.inputChangeHandler}
+                        // onChange = {this.handleName}
                     />
                     <p className = {'login_paraghraph'}>Age:</p>
                     <Input 
                         type = {'number'}
+                        name = 'age'
                         className = {'input_login'}
                         placeholder = {'Enter your age...'}
-                        onChange = {this.handleAge}
+                        // onChange = {this.handleAge}
+                        onChange={this.inputChangeHandler}
                     />
                     <p className = {'login_paraghraph'} >Email:</p>
                     <Input 
                         type = {'email'}
+                        name = 'email'
                         className = {'input_login'}
                         placeholder = {'Enter your email...'}
-                        onChange = {this.handleEmail}
+                        // onChange = {this.handleEmail}
+                        onChange={this.inputChangeHandler}
                     />
                     <p className = {'login_paraghraph'}>Phone:</p>
                     <Input 
                         type = {'number'}
+                        name = 'number'
                         className = {'input_login'}
                         placeholder = {'Enter your phone...'}
-                        onChange = {this.handlePhone}
+                        // onChange = {this.handlePhone}
+                        onChange={this.inputChangeHandler}
                     />
-                    <Button className = 'button' >SUBMIT</Button>
+                    <Button className = 'button' type = 'submit'>SUBMIT</Button>
                 </div>
             </form>
         )  
